2687f9d67c765203206642f2cdc6f31d29193500
Cocahuana <ezequieldominguez11@gmail.com>
2023-02-26 22:18:09 -0300

    Feat: add animation when change between pages

--- src/components/layouts/Default/Default.tsx
+++ src/components/layouts/Default/Default.tsx
@@ -1,13 +1,35 @@
 import Header from "@/components/modules/Header/Header";
 import Footer from "@/components/modules/Footer/Footer";
+import { motion } from "framer-motion";
 type Props = {
 	children: React.ReactNode;
 };
 
+const opacityVariant = {
+	initial: {
+		opacity: 0,
+	},
+	in: {
+		opacity: 1,
+	},
+	out: {
+		opacity: 0,
+	},
+};
+
+const animationTransition = { type: "spring", stiffness: 260, damping: 150 };
+
 const Default = ({ children }: Props) => (
 	<div>
 		<Header />
-		{children}
+		<motion.div
+			initial='initial'
+			animate='in'
+			variants={opacityVariant}
+			transition={animationTransition}
+		>
+			{children}
+		</motion.div>
 		<Footer />
 	</div>
 );
