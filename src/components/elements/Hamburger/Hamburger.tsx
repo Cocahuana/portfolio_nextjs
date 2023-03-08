3ba09d37e3c7971b6ae5b00330e3cf5cb0608c41
Cocahuana <ezequieldominguez11@gmail.com>
2023-03-08 18:40:43 -0300

    Feat: change some styles navbar

--- src/components/elements/Hamburger/Hamburger.tsx
+++ src/components/elements/Hamburger/Hamburger.tsx
@@ -1,10 +1,20 @@
-function Hamburger() {
+type Props = {
+	children?: React.ReactNode;
+	isClosed?: boolean;
+};
+
+export function Hamburger({ children, isClosed }: Props) {
 	return (
 		<div>
-			<button className='relative group'>
-				<div className='relative flex items-center justify-center w-[50px] h-[50px] transform transition-all bg-white  duration-200'>
-					<div className='flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 group-focus:-rotate-[45deg] origin-center'>
-						<HamburgerLines />
+			<button className='relative'>
+				<div
+					className={`relative flex items-center justify-center w-[50px] h-[50px] transform transition-all duration-200`}
+				>
+					<div
+						className={`flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300  origin-center 
+						${isClosed === true ? "-rotate-[45deg]" : "focus:-rotate-[45deg]"}`}
+					>
+						{isClosed === true ? children : <Lines />}
 					</div>
 				</div>
 			</button>
@@ -12,12 +22,20 @@ function Hamburger() {
 	);
 }
 
-const HamburgerLines = () => (
+const Lines = () => (
 	<>
-		<div className='bg-[color:var(--primary-bg)] h-[2px] w-1/2 rounded transform transition-all duration-300 group-focus:-rotate-90 group-focus:h-[1px] origin-right delay-75 group-focus:-translate-y-[1px]' />
+		<div className='bg-[color:var(--primary-bg)] h-[2px] w-1/2 rounded ' />
 		<div className='bg-[color:var(--primary-bg)] h-[1px] rounded' />
-		<div className='bg-[color:var(--primary-bg)] h-[2px] w-1/2 rounded self-end transform transition-all duration-300 group-focus:-rotate-90 group-focus:h-[1px] origin-left delay-75 group-focus:translate-y-[1px]' />
+		<div className='bg-[color:var(--primary-bg)] h-[2px] w-1/2 rounded self-end' />
 	</>
 );
 
-export default Hamburger;
+export const HamburgerClosed = () => (
+	<>
+		<div
+			className={`bg-[color:var(--primary-bg)] w-1/2 rounded transform transition-all duration-300 -rotate-90 h-[1px] origin-right delay-75 -translate-y-[1px]`}
+		/>
+		<div className='bg-[color:var(--primary-bg)] h-[1px] rounded' />
+		<div className='bg-[color:var(--primary-bg)]  w-1/2 rounded self-end transform transition-all duration-300 -rotate-90 h-[1px] origin-left delay-75 translate-y-[1px]' />
+	</>
+);
--- src/components/modules/Header/Header.tsx
+++ src/components/modules/Header/Header.tsx
@@ -1,13 +1,23 @@
 import NavLink from "@/components/elements/NavLink/NavLink";
 import SquareBlock from "@/components/elements/SquareBlock/SquareBlock";
-import Hamburger from "@/components/elements/Hamburger/Hamburger";
+import {
+	Hamburger,
+	HamburgerClosed,
+} from "@/components/elements/Hamburger/Hamburger";
 import { useState, useEffect } from "react";
 import Flex from "@/components/elements/Flex/Flex";
+import { motion, AnimatePresence } from "framer-motion";
 function Header() {
-	const [isOpen, setIsOpen] = useState(false);
-	const handleMenu = (event: React.MouseEvent<HTMLDivElement>) => {
+	const [open, setOpen] = useState(false);
+
+	const isOpen = (event: React.MouseEvent<HTMLDivElement>) => {
 		event.preventDefault();
-		setIsOpen(!isOpen);
+		setOpen(true);
+	};
+
+	const closeMenu = (event: React.MouseEvent<HTMLDivElement>) => {
+		event.preventDefault();
+		setOpen(false);
 	};
 	const navLinks = [
 		{
@@ -39,31 +49,62 @@ function Header() {
 
 	return (
 		<>
-			{isOpen === true ? (
-				<div className='z-50 w-full h-full fixed bg-[color:var(--skin-bg)]'>
-					<Flex className=' w-full h-full flex-col'>
-						<Flex
-							className='w-full h-[10vh] items-center justify-end px-6'
-							onClick={handleMenu}
-						>
-							<Hamburger />
-						</Flex>
-						<Flex className='w-full h-full items-center'>
-							<Flex className='flex-col w-full h-2/5 items-center justify-around'>
-								{navLinks.map((navLink) => (
-									<NavLink
-										className='text-2xl font-["DIN-Heavy"]'
-										key={navLink.label}
-										href={navLink.href}
-										label={navLink.label}
-									/>
-								))}
+			{open === true ? (
+				<AnimatePresence mode='wait'>
+					<motion.div
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+						transition={{ duration: 0.4 }}
+					>
+						<div className='z-50 w-full h-full fixed bg-[color:var(--skin-bg)]'>
+							<Flex className=' w-full h-full flex-col '>
+								<Flex
+									className='w-full h-[10vh] items-center justify-end px-6'
+									onClick={closeMenu}
+								>
+									<Hamburger isClosed={open}>
+										<HamburgerClosed />
+									</Hamburger>
+								</Flex>
+								<Flex className='w-full h-full items-center'>
+									<Flex className='flex-col w-full h-2/5 items-center justify-around'>
+										{navLinks.map((navLink, key) => (
+											<motion.a
+												key={key}
+												initial={{ y: -80, opacity: 0 }}
+												animate={{ y: 0, opacity: 1 }}
+												transition={{
+													delay: 0.4,
+													ease: "easeInOut",
+												}}
+												exit={{
+													opacity: 0,
+													y: -1000,
+													transition: {
+														ease: "easeInOut",
+														delay: 1,
+													},
+												}}
+											>
+												<NavLink
+													className='text-2xl font-["DIN-Heavy"]'
+													key={navLink.label}
+													href={navLink.href}
+													label={navLink.label}
+												/>
+											</motion.a>
+										))}
+									</Flex>
+								</Flex>
 							</Flex>
-						</Flex>
-					</Flex>
-				</div>
+						</div>
+					</motion.div>
+				</AnimatePresence>
 			) : (
-				<div className='h-[10vh] flex w-full bg-white fixed top-0 overflow-hidden'>
+				<div
+					className={`h-[10vh] flex w-full bg-white fixed top-0 overflow-hidden`}
+				>
 					<div className='flex lg:hidden w-full justify-between px-6 items-center'>
 						<div className='flex flex-col items-start'>
 							<div className='flex justify-center items-center'>
@@ -74,8 +115,10 @@ function Header() {
 								FULL STACK DEVELOPER
 							</p>
 						</div>
-						<div className='z-50' onClick={handleMenu}>
-							<Hamburger />
+						<div className='z-50' onClick={isOpen}>
+							<Hamburger isClosed={open}>
+								<HamburgerClosed />
+							</Hamburger>
 						</div>
 					</div>
 					<div className='hidden lg:flex w-full bg-white'>
