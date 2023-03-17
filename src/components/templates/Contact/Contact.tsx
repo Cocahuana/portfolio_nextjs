d58aad191084a2c820381905a10c53bdddf84160
Cocahuana <ezequieldominguez11@gmail.com>
2023-03-17 11:45:18 -0300

    Feat: centered Contacto form

--- src/components/templates/Contact/Contact.tsx
+++ src/components/templates/Contact/Contact.tsx
@@ -107,8 +107,8 @@ function Contacto() {
 	};
 
 	return (
-		<BoxSkin className='min-h-screen px-6'>
-			<Flex className='h-full justify-evenly flex-col gap-8 md:max-w-[724px]'>
+		<BoxSkin className='min-h-screen px-6  flex md:justify-center md:items-center'>
+			<Flex className='h-full justify-evenly md:justify-center md:items-center  flex-col gap-8 md:max-w-[724px]'>
 				<Flex className='items-center gap-3 justify-center'>
 					<SquareBlock />
 					<h3 className='text-2xl'>Platiquemos</h3>
