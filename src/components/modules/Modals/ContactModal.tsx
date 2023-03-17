5f4a2c248799e2c291ea8af8d05268f7fa042d7f
Cocahuana <ezequieldominguez11@gmail.com>
2023-03-17 09:21:01 -0300

    Feat: Modal pop up finish for contact section Mobile view

--- src/components/modules/Modals/ContactModal.tsx
+++ src/components/modules/Modals/ContactModal.tsx
@@ -2,7 +2,8 @@ import { motion } from "framer-motion";
 import Backdrop from "../Backdrop/Backdrop";
 import Flex from "@/components/elements/Flex/Flex";
 import { AiOutlineCloseCircle } from "react-icons/ai";
-
+import { BsCheckCircle } from "react-icons/bs";
+import Button from "@/components/elements/Button/Button";
 const dropIn = {
 	hidden: {
 		y: "-100vh",
@@ -35,22 +36,27 @@ function ContactModal({ handleClose, text }: any) {
 				animate='visible'
 				exit='exit'
 			>
-				<Flex className='flex-col relative min-h-[400px] flex-1 '>
-					<Flex className='bg-transparent justify-end items-center'>
-						<Flex className=' w-8 h-8 items-center justify-center'>
+				<Flex className='flex-col relative min-h-[400px]  '>
+					<Flex className='bg-transparent justify-end items-center p-2 px-6'>
+						<Flex className=' w-6 h-6 items-center justify-end'>
 							<AiOutlineCloseCircle
 								className='w-10 h-10 fill-white'
 								onClick={handleClose}
+								cursor='pointer'
 							/>
 						</Flex>
 					</Flex>
-					<Flex className='bg-green-400 rounded-t-md relative flex-1'>
-						NICE Icon
+					<Flex className='bg-green-500 rounded-t-md flex-1 items-center justify-center'>
+						<BsCheckCircle className='w-auto h-28 fill-white' />
 					</Flex>
-					<Flex className='bg-white rounded-b-md items-stretch flex-col relative flex-1'>
-						<h3>Todo Joya!</h3>
-						<p>{text}</p>
-						<button onClick={handleClose}>Close</button>
+					<Flex className='bg-white rounded-b-md flex-col items-center flex-1 gap-3 p-6 max-h-[160px]'>
+						<h3 className='text-2xl'>Todo Joya!</h3>
+						<p className='text-base'>{text}</p>
+						<Button
+							text='Cerrar'
+							active={true}
+							onClick={handleClose}
+						/>
 					</Flex>
 				</Flex>
 			</motion.div>
