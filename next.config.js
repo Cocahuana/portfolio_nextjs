33c164c7dec05ba6f7a32ff6281e6e4910e5e507
Cocahuana <ezequieldominguez11@gmail.com>
2024-03-18 23:27:31 -0300

    Fix: babelsrc error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // output: "export", // Exportar app cuando buildea (para hostinger)
-}
-
-module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {   
+  distDir: "build",   
+  output: "export", 
+};  
+module.exports = nextConfig;
+ 
